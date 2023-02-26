9e7045e1f174f320d84810ae36ea4ac47cdc8bad
Ambili P A <99420180+Ambiliambu@users.noreply.github.com>
2023-02-26 07:37:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,20 +4,17 @@ const path=require('path')
 const port=process.env.PORT || 5000;
 const connectDB=require('./config/db')
 const app=express()
-
+const cors = require('cors')
 
 connectDB()
 
 
-const cors = require('cors')
-
-app.use(cors())
-
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-
-app.get((req,res)=>{
+app.use(cors())
+app.get('/',(req,res)=>{
+    res.setHeader('Access-Control-Allow-Credentials','true');
     res.send('Api running')
 })
 
